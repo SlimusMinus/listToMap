ce3dd1a56894aee5fa42bc76368e1b4dc21f8561
SlimusMinus <moscow_city231@rambler.ru>
2025-02-24 10:34:39 +0300

    Solution

--- ListToMap.java
+++ ListToMap.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class ListToMap {
+
+  public static void main(String[] args) {
+
+    //Example on Java
+    List<Integer> list = List.of(1, 3, 4, 5, 1, 5, 4);
+    Map<Integer, Long> collect = list.stream()
+        .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+    collect.forEach((key, value) -> System.out.println(key + " : " + value));
+
+    //Example on Groovy
+    def list = [1, 3, 4, 5, 1, 5, 4]
+    def collect = list.stream()
+        .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+    collect.forEach {key, value -> println "$key -> $value"}
+
+  }
 }
